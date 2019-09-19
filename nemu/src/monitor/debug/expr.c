6660506eafa046a52771b99e1400ed663f47ceeb
tracer-ics2019 <tracer@njuics.org>
2019-09-19 21:40:38 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:40:38 up  3:13,  5 users,  load average: 0.24, 0.10, 0.09 5616a0e1db3b6d29cad9cb54fef3f8cd57935d9

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -33,7 +33,7 @@ static struct rule {
   
 };
 
-#define ADD_TO_TOKENS (tokens[nr_token].type = rules[i].token_type;  copy_char_array(rules[i].regex, tokens[nr_token].str;) 
+#define ADD_TO_TOKENS tokens[nr_token].type = rules[i].token_type;  copy_char_array(rules[i].regex, tokens[nr_token].str)  
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
 static regex_t re[NR_REGEX] = {};
 
@@ -95,14 +95,14 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-			case TK_NOTYPE: { tokens[nr_token].type = rules[i].token_type;  copy_char_array(rules[i].regex, tokens[nr_token].str); nr_token++; break; } 
-			case '+': { tokens[nr_token].type = rules[i].token_type;  copy_char_array(rules[i].regex, tokens[nr_token].str); nr_token++; break; }
-			case TK_EQ: { tokens[nr_token].type = rules[i].token_type;  copy_char_array(rules[i].regex, tokens[nr_token].str); nr_token++; break;  } 
-			case '-': { tokens[nr_token].type = rules[i].token_type;  copy_char_array(rules[i].regex, tokens[nr_token].str); nr_token++; break; }
-			case '*': { tokens[nr_token].type = rules[i].token_type;  copy_char_array(rules[i].regex, tokens[nr_token].str); nr_token++; break; } 
-			case '/': { tokens[nr_token].type = rules[i].token_type;  copy_char_array(rules[i].regex, tokens[nr_token].str); nr_token++; break; } 
-			case '(': { tokens[nr_token].type = rules[i].token_type;  copy_char_array(rules[i].regex, tokens[nr_token].str); nr_token++; break; } 
-			case ')': { tokens[nr_token].type = rules[i].token_type;  copy_char_array(rules[i].regex, tokens[nr_token].str); nr_token++; break; } 
+			case TK_NOTYPE: { ADD_TO_TOKENS; nr_token++; break; } 
+			case '+': { ADD_TO_TOKENS; nr_token++; break; }
+			case TK_EQ: { ADD_TO_TOKENS; nr_token++; break;  } 
+			case '-': { ADD_TO_TOKENS; nr_token++; break; }
+			case '*': { ADD_TO_TOKENS; nr_token++; break; } 
+			case '/': { ADD_TO_TOKENS; nr_token++; break; } 
+			case '(': { ADD_TO_TOKENS; nr_token++; break; } 
+			case ')': { ADD_TO_TOKENS; nr_token++; break; } 
           default: TODO();
         }
 

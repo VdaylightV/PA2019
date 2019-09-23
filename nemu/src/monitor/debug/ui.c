1d4f5fe584aed88169762fc1a9198e812f96d797
tracer-ics2019 <tracer@njuics.org>
2019-09-23 15:13:34 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  15:13:34 up  1:17,  5 users,  load average: 0.05, 0.04, 0.06 c2cca212b83a5105408e016c1c77ec976d3ea564

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -235,7 +235,7 @@ void show_array(Token *array) {
 */
 
 static int cmd_p(char *args) {
-//    Token *ptr = &tokens[0];
+    Token *ptr = &tokens_copy[0];
 //	show_array(ptr);
 //  make_token(args);
     char *arg = strtok(NULL,"$");
@@ -248,7 +248,7 @@ static int cmd_p(char *args) {
 	    printf("failed");
 	}
     
-  //  uint32_t product = eval(0, token_end, ptr);
+    uint32_t product = eval(0, token_end, ptr);
     Token *pointer = &tokens_copy[0];
 	printf("length:%u\n",token_end);
 //	int j = 0;
@@ -271,7 +271,7 @@ static int cmd_p(char *args) {
 		printf("\n");
 	}
 
-	//printf("%u\n", product);
+	printf("%u\n", product);
 
 
 

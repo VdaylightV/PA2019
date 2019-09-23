2a38e7a7894d8216d8e8fdcae5931d18d1a0d558
tracer-ics2019 <tracer@njuics.org>
2019-09-23 22:09:17 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:09:17 up  1:29,  4 users,  load average: 0.17, 0.15, 0.10 afd3d15fd3cf60a63ae55a5bc9c521fbf8cffa

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -247,10 +247,10 @@ static int cmd_p(char *args) {
 	else {
 	    printf("failed");
 	}
-    uint32_t bracket = check_parentheses(0, token_end-1, ptr);  
+//    uint32_t bracket = check_parentheses(0, token_end-1, ptr);  
     uint32_t product = eval(0, token_end-1, ptr);
 //    uint32_t op = find_op(0, token_end-1, ptr);
-    Token *pointer = &tokens_copy[0];
+//    Token *pointer = &tokens_copy[0];
 	printf("length:%u\n",token_end);
 //	int j = 0;
 	printf("content:\n");
@@ -262,7 +262,7 @@ static int cmd_p(char *args) {
 //	printf("\n");
 
 //	printf("begin type:%d\n",pointer[0].type);
-	for ( int i = 0; i < token_end; i ++ ) {
+/*	for ( int i = 0; i < token_end; i ++ ) {
 	    int j = 0;
 		while ( pointer[i].str[j] != '\0' ) {
 	     	printf("%c", pointer[i].str[j]);
@@ -271,9 +271,9 @@ static int cmd_p(char *args) {
 		}
 		printf("\n");
 	}
-	
+*/	
 //To print the results that was recorded in the tokens
-	printf("bracket:%u\n", bracket);
+//	printf("bracket:%u\n", bracket);
 //	printf("op:%u\n", op);
 	printf("product:%u\n",product);
 

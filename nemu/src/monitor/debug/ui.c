bc8256a32e1e98f692055b37fff67dfbc73f6f1a
tracer-ics2019 <tracer@njuics.org>
2019-09-23 21:56:30 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:56:30 up  1:16,  4 users,  load average: 0.06, 0.11, 0.09 dc0c626194f2c19bb58105593922ac6c5e3c410

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -248,7 +248,7 @@ static int cmd_p(char *args) {
 	    printf("failed");
 	}
     uint32_t bracket = check_parentheses(0, token_end-1, ptr);  
-    uint32_t product = eval(0, token_end-1, ptr);
+//    uint32_t product = eval(0, token_end-1, ptr);
     uint32_t op = find_op(0, token_end-1, ptr);
     Token *pointer = &tokens_copy[0];
 	printf("length:%u\n",token_end);
@@ -275,7 +275,7 @@ static int cmd_p(char *args) {
 //To print the results that was recorded in the tokens
 	printf("bracket:%u\n", bracket);
 	printf("op:%u\n", op);
-	printf("product:%u\n",product);
+//	printf("product:%u\n",product);
 
 	return 0;
 /*	printf("Hi\n");

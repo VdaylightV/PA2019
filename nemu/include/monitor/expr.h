91d953b540267470c46f35029eea73688f4b525c
tracer-ics2019 <tracer@njuics.org>
2019-09-23 09:12:43 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  09:12:43 up 38 min,  5 users,  load average: 0.14, 0.13, 0.16 879c39976af881894d74487284abde10aa955b2d

--- nemu/include/monitor/expr.h
+++ nemu/include/monitor/expr.h
@@ -14,4 +14,12 @@ uint32_t expr(char *, bool *);
 
 extern Token tokens[32];
 
+char *find_end(char *args);
+
+uint32_t check_parentheses(uint32_t p, uint32_t q, Token *token);
+
+uint32_t find_op(uint32_t p, uint32_t q, Token *token);
+
+uint32_t eval(uint32_t p, uint32_t q, Token *token);
+
 #endif

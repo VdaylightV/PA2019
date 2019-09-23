54906126fa5dd5caa1ff1f5c9f4ebe82293f5110
tracer-ics2019 <tracer@njuics.org>
2019-09-23 19:01:11 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:01:11 up 32 min,  4 users,  load average: 0.40, 0.15, 0.17 76811290ec83e55cd1a1ac0b2b43940fb387650d

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -262,7 +262,7 @@ static int cmd_p(char *args) {
 //	printf("\n");
 
 //	printf("begin type:%d\n",pointer[0].type);
-	for ( int i = 0; i < last_token; i ++ ) {
+	for ( int i = 0; i <= last_token; i ++ ) {
 	    int j = 0;
 		while ( pointer[i].str[j] != '\0' ) {
 	     	printf("%c", pointer[i].str[j]);

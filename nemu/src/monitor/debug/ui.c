abb40523004bb348b6aae8ae9e6180cae1c2ef5c
tracer-ics2019 <tracer@njuics.org>
2019-09-23 13:17:48 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  13:17:48 up 55 min,  3 users,  load average: 0.06, 0.21, 0.37 c2a8bbdd97210e448ed2af1e755c93e872c6d52b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -251,7 +251,12 @@ static int cmd_p(char *args) {
   //  uint32_t product = eval(0, token_end, ptr);
     Token *pointer = &tokens[0];
 	printf("length:%u\n",token_end);
-	printf("begin type:%d\n",pointer[0].type);
+	int j = 0;
+	while ( pointer[0].str[j] != '\0' ) {
+	    printf("%c",pointer[0].str[j]);
+		j ++;
+	}
+//	printf("begin type:%d\n",pointer[0].type);
 /*	for ( int i = 0; i <= token_end; i++ ) {
 	    int j = 0;
 		while ( pointer[i].str[j] != '\0' ) {

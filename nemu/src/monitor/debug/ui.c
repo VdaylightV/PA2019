42cca065d74431bd09ceba9395181b6b8ad6a992
tracer-ics2019 <tracer@njuics.org>
2019-09-23 22:00:33 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:00:33 up  1:20,  4 users,  load average: 0.03, 0.11, 0.09 fb4e0e0a17af7cd78f6d42eac4c7c524fbea9a9

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -235,7 +235,7 @@ void show_array(Token *array) {
 */
 
 static int cmd_p(char *args) {
-//	Token *ptr = &tokens_copy[0];
+	Token *ptr = &tokens_copy[0];
 //	show_array(ptr);
 //  make_token(args);
     char *arg = strtok(NULL,"$");
@@ -247,7 +247,7 @@ static int cmd_p(char *args) {
 	else {
 	    printf("failed");
 	}
-//    uint32_t bracket = check_parentheses(0, token_end-1, ptr);  
+    uint32_t bracket = check_parentheses(0, token_end-1, ptr);  
 //    uint32_t product = eval(0, token_end-1, ptr);
 //    uint32_t op = find_op(0, token_end-1, ptr);
     Token *pointer = &tokens_copy[0];
@@ -273,7 +273,7 @@ static int cmd_p(char *args) {
 	}
 	
 //To print the results that was recorded in the tokens
-//	printf("bracket:%u\n", bracket);
+	printf("bracket:%u\n", bracket);
 //	printf("op:%u\n", op);
 //	printf("product:%u\n",product);
 

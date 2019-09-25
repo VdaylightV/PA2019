96f00ce41b3f8bf414d488a32a45808e1bf41836
tracer-ics2019 <tracer@njuics.org>
2019-09-25 21:19:22 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:19:22 up  2:14,  5 users,  load average: 0.21, 0.08, 0.02 c66763e2fdbc54333b6c7248e5ec20a551d235

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,5 +1,12 @@
+#include "stdio.h"
+#include "string.h"
+#include "stdint.h"
+#include "stdbool.h"
+
+
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
+uint32_t expr(char *e, bool *success);  
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -7,6 +14,21 @@ int main(int argc, char *argv[]) {
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
+  
+  static char str[65536];
+  char *string = &str[0];
+  FILE *fp = fopen("/ics2019/nemu/tools/gen-expr/input","r");
+  while (!feof(fp)) {
+      fgets(string,65536,fp);
+	  printf("%s",string);
+  }
+  char *arg = strtok(string,"$");
+  arg = strtok(NULL,"$");
+  bool success = true;
+  uint32_t result = expr(arg,&success);
+  fclose(fp);
+  printf("%u\n",result);
+  
 
   return 0;
 }

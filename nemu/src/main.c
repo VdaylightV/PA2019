da83b373f9e3c6d501f5bbee7164954a45431898
tracer-ics2019 <tracer@njuics.org>
2019-09-26 13:43:54 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  13:43:54 up 41 min,  5 users,  load average: 0.20, 0.13, 0.19 db1c0837c4bfd20330d5fdaf3c43ea6429c32d25

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -24,9 +24,9 @@ int main(int argc, char *argv[]) {
     while ( !feof(fp)) {
       fgets(str,500,fp);
 	 // fscanf(fp,"%s",&str[0]);
-//	  int len;
-//	  len = strlen(str);
-	  //str[len-1] = '\0';
+	  int len;
+	  len = strlen(str);
+	  str[len-1] = '\0';
 	  printf("%s \n", str);		  
 
  // int j = 20;

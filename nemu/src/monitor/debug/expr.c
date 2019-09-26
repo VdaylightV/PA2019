11259432384e246b6771f43eb60df33840c2387c
tracer-ics2019 <tracer@njuics.org>
2019-09-26 21:54:59 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:54:59 up  3:38,  5 users,  load average: 0.38, 0.34, 0.35 d9f1ac95df363738d5a9cf7c18b62df4ac678588

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -44,7 +44,7 @@ static struct rule {
   {"\\)", ')'},						 // right_bracket
   {"[0-9]+",TK_NUM},			     // number
   {"[u]{1}",TK_CHAR},			     // character
-  {"^[0]{1}[x]{1}[0-9a-fA-F]+",TK_HEX},   // hex_number
+  {"^[0][x][0-9a-fA-F]+",TK_HEX},   // hex_number
  // {"[^\\$][0-9a-zA-Z]+",TK_REG},        // register
   {"&&", TK_AND},                    // and     
 };

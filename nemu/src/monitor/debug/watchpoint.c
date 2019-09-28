05a6c414f45ce92de21d9bc692e9f4bf3655149d
181240014-Gan Chen <181240014@smail.nju.edu.cn>
2019-09-28 16:29:26 +0800

    "Finish the organization of watchpoints"

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -11,6 +11,8 @@ void init_wp_pool() {
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = &wp_pool[i + 1];
+	wp_pool[i].work_state = false;
+	wp_pool[i].str[0] = '\0';
   }
   wp_pool[NR_WP - 1].next = NULL;
 
@@ -18,6 +20,52 @@ void init_wp_pool() {
   free_ = wp_pool;
 }
 
+WP *new_wp() {
+   index = 0
+   WP *follow_free_ = NULL;
+   while ( (*free_).work_state != false && index != 32 ) {
+       follow_free_ = free_;
+	   free_ = *free_.next;
+	   index ++;
+   }
+
+   assert(index != 32);
+   WP *temp = free_;
+   *temp.work_state = true;
+   
+   if ( follow_free_ == NULL ) {
+       free_ = *free_.next;
+	   return temp;
+   }
+
+   else if ( *free_.next != NULL ) {
+       *follow_free_.next = *free_.next;
+       free_ = wp_pool;	   
+	   return temp;
+   }
+
+   else if ( free_.next == NULL ) {
+	   *follow_free_.next = NULL;
+	   free_ = wp;
+	   return temp;
+   }
+
+   else {
+	    assert(0);
+	}
+}
+
+void free_wp(WP *wp) {
+    (*wp).work_state = false;
+	WP *tail = free_;
+	while( *tail.next != NULL ) {
+	    tail = *tail.next;
+	}
+
+	tail.next = wp;
+	tail = wp;
+	tail.next = NULL;
+}
 /* TODO: Implement the functionality of watchpoint */
 
 

a41d5673e3f8409a7db7b76d852738e0aada6af7
181240014-Gan Chen <181240014@smail.nju.edu.cn>
2019-09-28 16:30:35 +0800

    "WP version1.0-First cahnge"

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -2,9 +2,14 @@
 #define __WATCHPOINT_H__
 
 #include "common.h"
+#include "stdint.h"
 
 typedef struct watchpoint {
   int NO;
+  bool work_state;
+  char var_name[32]; 
+  uint32_t Old_Value;
+  uint32_t New_Value;
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */

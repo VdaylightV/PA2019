074cbc679356dd53091bccc4d1036122ad4ef2c3
tracer-ics2019 <tracer@njuics.org>
2019-10-13 21:27:16 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:27:16 up  2:25,  7 users,  load average: 0.02, 0.04, 0.07 e9575946cc230292eaa4dc1699c7560bb79f399c

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -26,7 +26,10 @@ static inline void rtl_sr(int r, const rtlreg_t* src1, int width) {
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-  TODO();
+  uint32_t temp = cpu.esp;
+  cpu.esp = temp - 4;
+
+  // TODO();
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {

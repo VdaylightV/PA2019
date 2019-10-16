f1908d3fd46b393ef2be921f1ec457c2fe99e545
tracer-ics2019 <tracer@njuics.org>
2019-10-16 23:09:57 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  23:09:57 up  7:30, 12 users,  load average: 0.02, 0.05, 0.01 c9860fa532c42c6271303daacd6e0e16f91ef789

--- nemu/src/isa/x86/decode/decode.c
+++ nemu/src/isa/x86/decode/decode.c
@@ -31,7 +31,8 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-  TODO();
+  //TODO();
+  op->simm = (int32_t)instr_fetch(pc, op->width);
 
   rtl_li(&op->val, op->simm);
 
--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -1,6 +1,23 @@
 #include "cpu/exec.h"
 #include "cc.h"
 
+make_EHelper(push) {
+/*  if ((*id_src)->type == OP_TYPE_IMM) {
+      rtl_push(&((*id_src)->imm));
+
+	  print_asm("push %x", (*id_src)->imm);
+  }
+
+  if ((*id_sr)c->type == OP_TYPE_EWG) {
+      rtl_push(&((*id_src)->reg));
+
+      print_asm("push %x", (*id_src)->reg);
+  }
+*/
+	rtl_push(&decinfo.src.val);
+  assert(0);
+}
+
 make_EHelper(jmp) {
   // the target address is calculated at the decode stage
   rtl_j(decinfo.jmp_pc);
@@ -26,8 +43,9 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  TODO();
+  // TODO();
 
+  rtl_j(decinfo.jmp_pc);
   print_asm("call %x", decinfo.jmp_pc);
 }
 

3041eff65e85bf0129b977fa577d60bcfdc55835
tracer-ics2019 <tracer@njuics.org>
2019-10-18 20:13:26 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:13:26 up  1:20, 17 users,  load average: 0.19, 0.09, 0.09 17f0b5bff4a36a1416e05cfbd6028c386b8b935

--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -24,13 +24,6 @@ make_EHelper(jmp_rm) {
   print_asm("jmp *%s", id_dest->str);
 }
 
-/*
-make_EHelper(push) {
-	rtl_push(&id_src->val);
-
-}
-*/
-
 make_EHelper(call) {
   rtl_push(&decinfo.jmp_pc);
   // the target address is calculated at the decode stage
--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -28,7 +28,7 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   reg_l(R_ESP) -= 4;
-  rtl_sm(&reg_l(R_ESP), src1, 4);
+  vaddr_write(reg_l(R_ESP), *src1, 4);
 
     
   // TODO();

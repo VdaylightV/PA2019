5b211cc22c1b863ac867927b15b573ef573b2091
tracer-ics2019 <tracer@njuics.org>
2019-10-27 20:49:14 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:49:14 up 10:05, 21 users,  load average: 0.20, 0.13, 0.10 faa90891e639438c1dc679483576c87b502cbd37

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -137,7 +137,12 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  switch(width) {
+	  case 1: {*dest = ((uint32_t)(((int32_t)*src1) << 24) >> 24); break;}
+      case 2: {*dest = ((uint32_t)(((int32_t)*src1) << 16) >> 16); break;}
+	  default: assert(0);
+  }
+  //TODO();
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,

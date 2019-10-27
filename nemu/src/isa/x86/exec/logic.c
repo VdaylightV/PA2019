8735894951dab817631caadd521812f83db58e2a
tracer-ics2019 <tracer@njuics.org>
2019-10-27 21:05:31 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:05:31 up 10:21, 21 users,  load average: 0.04, 0.07, 0.08 4ff117867149b8128e2962fe27133b9c682360bb

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -19,7 +19,7 @@ make_EHelper(and) {
   id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
   rtl_sext(&s1, &id_src->val, id_src->width);
   */
-  printf("%u\n",id_src->val);
+  printf("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH%u\n",id_src->val);
   rtl_and(&s0, &id_dest->val, &id_src->val);
 
   operand_write(id_dest, &s0);

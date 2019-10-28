6e8bddaaa20534dcbbc22275137118951cacec12
tracer-ics2019 <tracer@njuics.org>
2019-10-28 18:22:36 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  18:22:36 up 14:57, 25 users,  load average: 0.41, 0.27, 0.16 ea3513e342fb4be1f1068cb0861f7e3add336d3c

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -4,42 +4,42 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   
   if(ref_r->edx != cpu.edx) {
-      printf("EDX doesn't match!");
+      printf("EDX doesn't match!!!!\n");
 	  return false;
   }
 
   if(ref_r->eax != cpu.eax) {
-      printf("EAX doesn't match!");
+      printf("EAX doesn't match!!!!\n");
 	  return false;
   }
 
   if(ref_r->ecx != cpu.ecx) {
-      printf("ECX doesn't match!");
+      printf("ECX doesn't match!!!!\n");
 	  return false;
   }
   
   if(ref_r->ebx != cpu.ebx) {
-      printf("EBX doesn't match!");
+      printf("EBX doesn't match!!!!\n");
 	  return false;
   }
 
   if(ref_r->esp != cpu.esp) {
-      printf("ESP doesn't match!");
+      printf("ESP doesn't match!!!!\n");
 	  return false;
   }
 
   if(ref_r->ebp != cpu.ebp) {
-      printf("EBP doesn't match!");
+      printf("EBP doesn't match!!!!\n");
 	  return false;
   }
 
   if(ref_r->esi != cpu.esi) {
-      printf("ESI doesn't match!");
+      printf("ESI doesn't match!!!!\n");
 	  return false;
   }
 
   if(ref_r->edi != cpu.edi) {
-      printf("EDI doesn't match!");
+      printf("EDI doesn't match!!!!\n");
 	  return false;
   }
 

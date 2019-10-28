02e748d76651bc3f71202e93f2456b9a7f1cee7c
tracer-ics2019 <tracer@njuics.org>
2019-10-28 17:35:25 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  17:35:25 up 14:49, 25 users,  load average: 0.25, 0.10, 0.07 6e689d472eb09fc2e2a7c68cfc49b4a9b1fccd0f

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -8,6 +8,40 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 	  return false;
   }
 
+  if(ref_r->eax != cpu.eax) {
+      printf("EAX doesn't match!");
+	  return false;
+  }
+
+  if(ref_r->ecx != cpu.ecx) {
+      printf("ECX doesn't match!");
+	  return false;
+  }
+  
+  if(ref_r->ebx != cpu.ebx) {
+      printf("EBX doesn't match!");
+	  return false;
+  }
+
+  if(ref_r->esp != cpu.esp) {
+      printf("ESP doesn't match!");
+	  return false;
+  }
+
+  if(ref_r->ebp != cpu.ebp) {
+      printf("EBP doesn't match!");
+	  return false;
+  }
+
+  if(ref_r->esi != cpu.esi) {
+      printf("ESI doesn't match!");
+	  return false;
+  }
+
+  if(ref_r->edi != cpu.edi) {
+      printf("EDI doesn't match!");
+	  return false;
+  }
 
   return true;
 }

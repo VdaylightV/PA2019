ed882f88c7187c038e4328f454537294c65cb0a4
tracer-ics2019 <tracer@njuics.org>
2019-10-28 19:46:44 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:46:44 up 16:21, 25 users,  load average: 0.01, 0.08, 0.05 dfead2562abd13c3d6e78fb5121f9749614d0104

--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -34,8 +34,8 @@ make_EHelper(call) {
 
 make_EHelper(ret) {
    
- // rtl_mv(&cpu.esp, &cpu.ebp);
-  rtl_pop(&t1);
+  //rtl_mv(&cpu.esp, &cpu.ebp);
+  //rtl_pop(&t1);
   
   rtl_lm(&t0, &cpu.esp, 4);
   rtl_j(t0);

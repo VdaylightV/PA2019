e512313bc473c8a3f4cc63a50fa5f7bc97bc8b3f
tracer-ics2019 <tracer@njuics.org>
2019-10-28 18:59:46 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  18:59:46 up 15:34, 25 users,  load average: 0.07, 0.04, 0.01 a4f4f0d290e954c704a9633bdedbe69d3dc2d8f

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -5,49 +5,49 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   
   if(ref_r->edx != cpu.edx) {
       printf("EDX doesn't match!!!!\n");
-	  printf("The correct value of edx: %x\n", ref_r->edx);
+	  printf("The correct value of edx: %x!!!!\n", ref_r->edx);
 	  return false;
   }
 
   if(ref_r->eax != cpu.eax) {
       printf("EAX doesn't match!!!!\n");
-	  printf("The correct value of eax: %x\n", ref_r->eax);
+	  printf("The correct value of eax: %x!!!!\n", ref_r->eax);
 	  return false;
   }
 
   if(ref_r->ecx != cpu.ecx) {
       printf("ECX doesn't match!!!!\n");
-	  printf("The correct value of ecx: %x\n", ref_r->ecx);
+	  printf("The correct value of ecx: %x!!!!\n", ref_r->ecx);
 	  return false;
   }
   
   if(ref_r->ebx != cpu.ebx) {
       printf("EBX doesn't match!!!!\n");
-	  printf("The correct value of ebx: %x\n", ref_r->ebx);
+	  printf("The correct value of ebx: %x!!!!\n", ref_r->ebx);
 	  return false;
   }
 
   if(ref_r->esp != cpu.esp) {
       printf("ESP doesn't match!!!!\n");
-	  printf("The correct value of esp: %x\n", ref_r->esp);
+	  printf("The correct value of esp: %x!!!!\n", ref_r->esp);
 	  return false;
   }
 
   if(ref_r->ebp != cpu.ebp) {
       printf("EBP doesn't match!!!!\n");
-	  printf("The correct value of ebp: %x\n", ref_r->ebp);
+	  printf("The correct value of ebp: %x!!!!\n", ref_r->ebp);
 	  return false;
   }
 
   if(ref_r->esi != cpu.esi) {
       printf("ESI doesn't match!!!!\n");
-	  printf("The correct value of esi: %x\n", ref_r->esi);
+	  printf("The correct value of esi: %x!!!!\n", ref_r->esi);
 	  return false;
   }
 
   if(ref_r->edi != cpu.edi) {
       printf("EDI doesn't match!!!!\n");
-	  printf("The correct value of edi: %x\n", ref_r->edi);
+	  printf("The correct value of edi: %x!!!!\n", ref_r->edi);
 	  return false;
   }
 

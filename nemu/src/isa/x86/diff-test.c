2ccb6d6847ec6a625f8ee5855a1a51495ecc5499
tracer-ics2019 <tracer@njuics.org>
2019-10-29 11:04:25 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  11:04:25 up 19:42, 28 users,  load average: 0.13, 0.12, 0.09 499351091d4e5044c8307bc234520288b5fd6717

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -6,7 +6,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->edx != cpu.edx) {
       printf("EDX doesn't match!!!!\n");
 	  printf("The correct value of edx: %x!!!!\n", ref_r->edx);
-	  printf("Correct EFLAGS: !!!!");
+	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
@@ -17,7 +17,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->eax != cpu.eax) {
       printf("EAX doesn't match!!!!\n");
 	  printf("The correct value of eax: %x!!!!\n", ref_r->eax);
-	  printf("Correct EFLAGS: !!!!");
+	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
@@ -29,7 +29,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->ecx != cpu.ecx) {
       printf("ECX doesn't match!!!!\n");
 	  printf("The correct value of ecx: %x!!!!\n", ref_r->ecx);
-	  printf("Correct EFLAGS: !!!!");
+	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
@@ -41,7 +41,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->ebx != cpu.ebx) {
       printf("EBX doesn't match!!!!\n");
 	  printf("The correct value of ebx: %x!!!!\n", ref_r->ebx);
-	  printf("Correct EFLAGS: !!!!");
+	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
@@ -53,7 +53,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->esp != cpu.esp) {
       printf("ESP doesn't match!!!!\n");
 	  printf("The correct value of esp: %x!!!!\n", ref_r->esp);
-	  printf("Correct EFLAGS: !!!!");
+	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
@@ -65,7 +65,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->ebp != cpu.ebp) {
       printf("EBP doesn't match!!!!\n");
 	  printf("The correct value of ebp: %x!!!!\n", ref_r->ebp);
-	  printf("Correct EFLAGS: !!!!");
+	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
@@ -77,7 +77,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->esi != cpu.esi) {
       printf("ESI doesn't match!!!!\n");
 	  printf("The correct value of esi: %x!!!!\n", ref_r->esi);
-	  printf("Correct EFLAGS: !!!!");
+	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
@@ -89,7 +89,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->edi != cpu.edi) {
       printf("EDI doesn't match!!!!\n");
 	  printf("The correct value of edi: %x!!!!\n", ref_r->edi);
-	  printf("Correct EFLAGS: !!!!");
+	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);

e7eeb377926df50975695618875ab3dc1f6853e6
tracer-ics2019 <tracer@njuics.org>
2019-10-30 09:32:24 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  09:32:23 up 1 day,  1:12, 28 users,  load average: 1.23, 1.15, 0.96 579a1529c3b501f8e9c6e44eb807de8d253c980b

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -6,95 +6,96 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->edx != cpu.edx) {
       printf("EDX doesn't match!!!!\n");
 	  printf("The correct value of edx: %x!!!!\n", ref_r->edx);
-	  printf("Correct EFLAGS: !!!!\n");
+/*	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
 	  printf("OF: %x\n",ref_r->eflags.OF);
+*/	  printf("\n");
 	  return false;
   }
 
   if(ref_r->eax != cpu.eax) {
       printf("EAX doesn't match!!!!\n");
 	  printf("The correct value of eax: %x!!!!\n", ref_r->eax);
-	  printf("Correct EFLAGS: !!!!\n");
+/*	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
 	  printf("OF: %x\n",ref_r->eflags.OF);
-	  printf("\n");
+*/	  printf("\n");
 	  return false;
   }
 
   if(ref_r->ecx != cpu.ecx) {
       printf("ECX doesn't match!!!!\n");
 	  printf("The correct value of ecx: %x!!!!\n", ref_r->ecx);
-	  printf("Correct EFLAGS: !!!!\n");
+/*	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
 	  printf("OF: %x\n",ref_r->eflags.OF);
-	  printf("\n");
+*/	  printf("\n");
 	  return false;
   }
   
   if(ref_r->ebx != cpu.ebx) {
       printf("EBX doesn't match!!!!\n");
 	  printf("The correct value of ebx: %x!!!!\n", ref_r->ebx);
-	  printf("Correct EFLAGS: !!!!\n");
+/*	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
 	  printf("OF: %x\n",ref_r->eflags.OF);
-	  printf("\n");
+*/	  printf("\n");
 	  return false;
   }
 
   if(ref_r->esp != cpu.esp) {
       printf("ESP doesn't match!!!!\n");
 	  printf("The correct value of esp: %x!!!!\n", ref_r->esp);
-	  printf("Correct EFLAGS: !!!!\n");
+/*	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
 	  printf("OF: %x\n",ref_r->eflags.OF);
-	  printf("\n");
+*/	  printf("\n");
 	  return false;
   }
 
   if(ref_r->ebp != cpu.ebp) {
       printf("EBP doesn't match!!!!\n");
 	  printf("The correct value of ebp: %x!!!!\n", ref_r->ebp);
-	  printf("Correct EFLAGS: !!!!\n");
+/*	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
 	  printf("OF: %x\n",ref_r->eflags.OF);
-	  printf("\n");
+*/	  printf("\n");
 	  return false;
   }
 
   if(ref_r->esi != cpu.esi) {
       printf("ESI doesn't match!!!!\n");
 	  printf("The correct value of esi: %x!!!!\n", ref_r->esi);
-	  printf("Correct EFLAGS: !!!!\n");
+/*	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
 	  printf("OF: %x\n",ref_r->eflags.OF);
-	  printf("\n");
+*/	  printf("\n");
 	  return false;
   }
 
   if(ref_r->edi != cpu.edi) {
       printf("EDI doesn't match!!!!\n");
 	  printf("The correct value of edi: %x!!!!\n", ref_r->edi);
-	  printf("Correct EFLAGS: !!!!\n");
+/*	  printf("Correct EFLAGS: !!!!\n");
 	  printf("ZF: %x\n",ref_r->eflags.ZF);
 	  printf("CF: %x\n",ref_r->eflags.CF);
 	  printf("SF: %x\n",ref_r->eflags.SF);
 	  printf("OF: %x\n",ref_r->eflags.OF);
-	  printf("\n");
+*/	  printf("\n");
 	  return false;
   }
 

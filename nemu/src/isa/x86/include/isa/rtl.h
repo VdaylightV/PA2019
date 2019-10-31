f2b580f31954f2a0a76a48f8db38bf7f952fca2f
tracer-ics2019 <tracer@njuics.org>
2019-10-31 20:46:29 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:46:29 up 1 day, 15:05, 25 users,  load average: 2.25, 1.83, 1.50 e10f4d518d2a566a7561d6ced60d7d860594cd9

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -69,7 +69,7 @@ static inline void rtl_is_sub_overflow(rtlreg_t* dest,
 
 static inline void rtl_is_sub_carry(rtlreg_t* dest,
     const rtlreg_t* res, const rtlreg_t* src1) {
-    rtl_setrelop(RELOP_LTU, dest, res, src1);
+    rtl_setrelop(RELOP_LTU, dest, src1, res);
 	/*
 	if (dest, &cpu.esp, 40 > *res && t0 > *src1) {
 	    *dest = 1;

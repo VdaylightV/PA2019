f34f50d6af1c2540beca418fac0a1b221ebc4caf
tracer-ics2019 <tracer@njuics.org>
2019-10-31 13:02:53 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  13:02:53 up 1 day, 10:38, 24 users,  load average: 1.34, 1.25, 1.21 76fab47c2ebbc4c8cf191aca25ffd4d607405

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -8,7 +8,8 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     CC_O, CC_NO, CC_B,  CC_NB,
     CC_E, CC_NE, CC_BE, CC_NBE,
     CC_S, CC_NS, CC_P,  CC_NP,
-    CC_L, CC_NL, CC_LE, CC_NLE
+    CC_L, CC_NL, CC_LE, CC_NLE,
+	CC_GE
   };
 
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
@@ -23,6 +24,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_LE:
 	case CC_NE: {*dest = ~cpu.eflags.ZF; break; }
 	case CC_NL: {if ( cpu.eflags.CF == 1 ) (*dest) = 0; else (*dest) = 1; printf("@@@@@____%u____@@@@@\n", *dest); break; }
+	case CC_GE: {if ( cpu.eflags.CF == 1 ) (*dest) = 0; else (*dest) = 1; printf("@@@@@____%u____@@@@@\n", *dest); break; }
       TODO();
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");

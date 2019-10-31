a75753a27f4d3a278feeb04d732019356d182d7d
tracer-ics2019 <tracer@njuics.org>
2019-10-31 15:47:50 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  15:47:49 up 1 day, 12:26, 25 users,  load average: 1.33, 1.25, 1.19 c11d9b0aac55511520b4177ddeb4ef4c9134a1f

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -20,7 +20,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_E: {*dest = cpu.eflags.ZF; break; }
     case CC_BE:
     case CC_S:
-    case CC_L: {if (cpu.eflags.CF == 0) (*dest) = 1; else (*dest) = 0; break; }
+    case CC_L: {if (cpu.eflags.OF != cpu.eflags.SF) (*dest) = 1; else (*dest) = 0; break; }
     case CC_LE:
 	case CC_NE: {*dest = ~cpu.eflags.ZF; break; }
 	case CC_NL: {if (cpu.eflags.SF == cpu.eflags.OF) (*dest) = 1; else (*dest) = 0; printf("@@@@@@@@@____%d____@@@@@@@@\n",(*dest)); break; }

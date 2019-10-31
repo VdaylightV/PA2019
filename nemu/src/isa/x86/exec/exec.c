73d4ae5809c114a75d466572b3c1c0173c98a004
tracer-ics2019 <tracer@njuics.org>
2019-10-31 12:58:19 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  12:58:19 up 1 day, 10:34, 24 users,  load average: 1.20, 1.15, 1.19 ada6be4061bb683e8bbc3401f1422f68116fa37f

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -66,7 +66,7 @@ static OpcodeEntry opcode_table [512] = {
   /* 0x28 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x2c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x30 */	EMPTY, IDEX(G2E, xor), EMPTY, EMPTY,
-  //* 0x34 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x34 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x38 */	EMPTY, IDEX(G2E, cmp), EMPTY, IDEX(G2E, cmp),
   /* 0x3c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x40 */	EMPTY, EMPTY, EMPTY, EMPTY,

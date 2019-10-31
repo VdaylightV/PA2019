312652470d86f75f2c51740afd0109ce2402ca3b
tracer-ics2019 <tracer@njuics.org>
2019-10-31 14:38:30 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  14:38:30 up 1 day, 11:25, 24 users,  load average: 1.18, 1.19, 1.18 de93f64342388fccc4a68c1b1427edd4365bc19f

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -29,8 +29,8 @@ make_EHelper(sub) {
 
 make_EHelper(cmp) {
   
- // id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
- // rtl_sext(&s1, &id_src->val, id_src->width);
+  id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
+  rtl_sext(&s1, &id_src->val, id_src->width);
   
   rtl_sub(&s0, &id_dest->val, &id_src->val);
   rtl_is_sub_carry(&cpu.eflags.CF, &s0, &id_dest->val);

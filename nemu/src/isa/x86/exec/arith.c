3f8ae6707b980123b4bf6800fff6c633ad9852a2
tracer-ics2019 <tracer@njuics.org>
2019-11-02 14:35:10 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  14:35:10 up 2 days,  2:46, 29 users,  load average: 2.41, 2.33, 2.24 54429b4e80252294f1069f6d3e1c2009d1bb9bfc

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -18,17 +18,19 @@ make_EHelper(sub) {
   rtl_sext(&s1, &id_src->val, id_src->width);
    
   rtl_sub(&s0, &id_dest->val, &id_src->val);
-  printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@minused:%x\n", id_dest->val);
+/*  printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@minused:%x\n", id_dest->val);
   printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@minus_before_extend:%x\n", id_src->val);
   printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@minused_after_extend:%x\n", s1);
   printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@result:%x\n", s0);
+*/ 
   rtl_is_sub_carry(&cpu.eflags.CF, &s0, &id_dest->val);
   rtl_is_sub_overflow(&cpu.eflags.OF, &s0, &id_dest->val, &id_src->val, id_src->width);
   rtl_update_ZFSF(&s0, id_dest->width);
-  printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@cpu.eflags.CF:%x\n", cpu.eflags.CF);
+/*  printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@cpu.eflags.CF:%x\n", cpu.eflags.CF);
   printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@cpu.eflags.OF:%x\n", cpu.eflags.OF);
   printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@cpu.eflags.ZF:%x\n", cpu.eflags.ZF);
   printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@cpu.eflags.SF:%x\n", cpu.eflags.SF);
+*/  
   operand_write(id_dest, &s0);
   printf("$$$$$$$$$$$$$$$$___cpu.esi:%x__$$$$$$$$$$$$$$$$$$\n",cpu.esi);
   //TODO();

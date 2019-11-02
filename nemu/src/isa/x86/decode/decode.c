24a87fcc7e503330d3fe3a2a9ac325e8eeb4b286
tracer-ics2019 <tracer@njuics.org>
2019-11-02 17:16:00 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  17:16:00 up 2 days,  5:27, 15 users,  load average: 2.21, 2.15, 2.10 62b5c9bd1f1453977862b0365cae5d9c3bcadde5

--- nemu/src/isa/x86/decode/decode.c
+++ nemu/src/isa/x86/decode/decode.c
@@ -44,7 +44,7 @@ static inline make_DopHelper(SI) {
 //  printf("SI:+++++++++++++++++++++++++The result:%x\n",op->imm);
 
   switch(op->width) {
-	  case 1: { s0 = ((uint8_t)instr_fetch(pc, 1)); rtl_sext(&s1, &s0, 1); op->simm = s1; break;}
+	  case 1: { s0 = ((uint16_t)instr_fetch(pc, 1)); rtl_sext(&s1, &s0, 1); op->simm = s1; break;}
 	  case 2: { s0 = ((uint16_t)instr_fetch(pc, 2)); rtl_sext(&s1, &s0, 2); op->simm = s1; break;}
 	  case 4: { s0 =  instr_fetch(pc, 4); op->simm = s0; break;}
 

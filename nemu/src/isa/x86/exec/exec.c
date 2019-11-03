6dbeb83b3d419ff4a75f2e11d5674752a9532862
tracer-ics2019 <tracer@njuics.org>
2019-11-03 13:09:07 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  13:09:07 up 2 days, 11:24, 21 users,  load average: 1.38, 1.33, 1.71 189bf612a42ce4af25b9ea45b64671b8a94fe6cc

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -31,7 +31,7 @@ make_group(gp2,
 
 /* 0xf6, 0xf7 */
 make_group(gp3,
-    IDEX(test_I, test), EMPTY, EMPTY, EMPTY,
+    IDEX(test_I, test), EX(not), EMPTY, EMPTY,
     EMPTY, EX(imul1), EMPTY, EX(idiv))
 
 /* 0xfe */

806c289dc7042378f1d346cbf380996af2c7ceca
tracer-ics2019 <tracer@njuics.org>
2019-11-03 19:25:05 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:25:05 up 2 days, 14:57, 22 users,  load average: 1.22, 2.16, 2.08 1de27430b7b1d8add780eda97ce28c025c2f858e

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -114,7 +114,7 @@ make_EHelper(shr) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
 
-  rtl_shl(&s0, &id_dest->val, &id_src->val);
+  rtl_shr(&s0, &id_dest->val, &id_src->val);
   rtl_update_ZFSF(&s0, id_dest->width);
   operand_write(id_dest, &s0);
   print_asm_template2(shr);

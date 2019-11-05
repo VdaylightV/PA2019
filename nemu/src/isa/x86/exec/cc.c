edc55c4c5f4ec2c14e237600caedcfef7915674f
tracer-ics2019 <tracer@njuics.org>
2019-11-05 11:32:17 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  11:32:17 up 2 days, 18:57, 27 users,  load average: 1.46, 1.75, 1.81 7c4f6b7adc560cbf5938e56c9361a9b4d60df52

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -15,11 +15,11 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
+    case CC_O:{if (cpu.eflags.OF == 1) (*dest) = 1; else (*dest) = 0; break; }
     case CC_B:{if (cpu.eflags.CF == 1) (*dest) = 1; else (*dest) = 0; break; }
     case CC_E: {*dest = cpu.eflags.ZF; break; }
     case CC_BE: {if (cpu.eflags.CF == 1 || cpu.eflags.ZF == 1) (*dest) = 1; else (*dest) = 0; break; }
-    case CC_S:
+    case CC_S: {if (cpu.eflags.SF == 1) (*dest) = 1; else (*dest) = 0; break; }
     case CC_L: {if (cpu.eflags.OF != cpu.eflags.SF) (*dest) = 1; else (*dest) = 0; break; }
     case CC_LE: {if (cpu.eflags.ZF == 1 || cpu.eflags.SF != cpu.eflags.OF) (*dest) = 1; else (*dest) = 0; break; }
 	case CC_NE: {*dest = ~cpu.eflags.ZF; break; }

5ef214a72178d5b86a56421f28ab765077d19c69
tracer-ics2019 <tracer@njuics.org>
2019-11-05 13:15:19 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  13:15:19 up 2 days, 19:38, 27 users,  load average: 4.17, 2.27, 1.76 4f23ea017286b52e53b4e46a8273c7ae712e78a0

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -15,8 +15,8 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:{if (cpu.eflags.OF == 1) (*dest) = 1; else (*dest) = 0; break; }
-    case CC_B:{if (cpu.eflags.CF == 1) (*dest) = 1; else (*dest) = 0; break; }
+    case CC_O: {if (cpu.eflags.OF == 1) (*dest) = 1; else (*dest) = 0; break; }
+    case CC_B:// {if (cpu.eflags.CF == 1) (*dest) = 1; else (*dest) = 0; break; }
     case CC_E: {*dest = cpu.eflags.ZF; break; }
     case CC_BE: {if (cpu.eflags.CF == 1 || cpu.eflags.ZF == 1) (*dest) = 1; else (*dest) = 0; break; }
     case CC_S: {if (cpu.eflags.SF == 1) (*dest) = 1; else (*dest) = 0; break; }

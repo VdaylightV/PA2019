e0daa3c9227dd6d35fb87196e513880a17ff1253
tracer-ics2019 <tracer@njuics.org>
2019-11-10 19:45:53 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:45:53 up 3 days, 17:32, 27 users,  load average: 2.05, 2.10, 2.01 bc8084cd66e00fa53a6885bd3d532786bf674071

--- nemu/src/isa/x86/decode/decode.c
+++ nemu/src/isa/x86/decode/decode.c
@@ -280,7 +280,7 @@ make_DHelper(a2O) {
 make_DHelper(J) {
   decode_op_SI(pc, id_dest, false);
   // the target address can be computed in the decode stage
-  printf("PC:%x\n", decinfo.seq_pc);
+//  printf("PC:%x\n", decinfo.seq_pc);
   decinfo.jmp_pc = id_dest->simm + *pc;
 //  printf("Jump_PC:%x\n",decinfo.jmp_pc);
 }

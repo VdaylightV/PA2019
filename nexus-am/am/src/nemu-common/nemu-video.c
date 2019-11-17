2dc71ddc90f551663f11617d8a55da1deda274e5
tracer-ics2019 <tracer@njuics.org>
2019-11-17 22:15:52 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:15:52 up 4 days, 21:30, 14 users,  load average: 2.29, 2.26, 2.05 aba9b2a9b24c6b46f7030746819e6bebf129d2ac

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -3,6 +3,12 @@
 #include <nemu.h>
 #include <klib.h>
 
+static inline int min(int x, int y) {
+  return (x < y) ? x : y;
+}
+
+static uint32_t fb[400 * 300] = {};
+
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
@@ -20,10 +26,17 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+      int cp_bytes = sizeof(uint32_t) * min(w, 400 - x);
+      for (int j = 0; j < h && y + j < 300; j ++) {
+        memcpy(&fb[(y + j) * 400 + x], pixels, cp_bytes);
+        pixels += w;
+      }
 
       if (ctl->sync) {
         //outl(SYNC_ADDR, 0);
-        outl(0x104, 0);
+       // outl(0x104, 0);
       }
       return size;
     }

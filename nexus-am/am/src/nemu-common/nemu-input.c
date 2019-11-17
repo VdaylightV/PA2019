2180b8fcd5bb123182300fcf9075aed6a803092b
tracer-ics2019 <tracer@njuics.org>
2019-11-17 14:39:43 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  14:39:43 up 4 days, 17:19, 12 users,  load average: 2.30, 2.10, 1.86 51364d635d739f4d5cf7ec3aad2c5534586bdf38

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -8,8 +8,13 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbd->keydown = 0;
       kbd->keycode = _KEY_NONE;
+      int input = inl(0x60);
+      if(input != _KEY_NONE) {
+          kbd->keycode = input;
+          kbd->keydown = !(kbd->keydown);
+      }
+
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }

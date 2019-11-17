646e70a0bbf8d3ea4efdb14e0241d0c25d7e58ff
tracer-ics2019 <tracer@njuics.org>
2019-11-17 22:26:27 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:26:27 up 4 days, 21:40, 14 users,  load average: 2.19, 2.08, 2.05 aa45796ff31158e85785bcdc28d22512ad9746be

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -7,7 +7,7 @@ static inline int min(int x, int y) {
   return (x < y) ? x : y;
 }
 
-//static uint32_t fb[400 * 300] = {};
+static uint32_t fb[400 * 300] = {};
 
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
@@ -26,17 +26,17 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-/*      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
       int cp_bytes = sizeof(uint32_t) * min(w, 400 - x);
       for (int j = 0; j < h && y + j < 300; j ++) {
         memcpy(&fb[(y + j) * 400 + x], pixels, cp_bytes);
         pixels += w;
       }
-*/
-      if (ctl->sync) {
+
+    if (ctl->sync) {
         //outl(SYNC_ADDR, 0);
-        outl(0x104, 0);
+//        outl(0x104, 0);
       }
       return size;
     }
@@ -45,10 +45,10 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_vga_init() {   
-  int i;
+/*  int i;
   int size = screen_width() *screen_height();
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < size; i ++) fb[i] = i;
   draw_sync();
-
+*/
 }

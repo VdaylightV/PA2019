3815c32d352c523833b1306965c34741ad356382
tracer-ics2019 <tracer@njuics.org>
2019-11-17 16:07:17 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  16:07:17 up 4 days, 18:47, 14 users,  load average: 2.67, 2.11, 2.00 bdd9f42522514d608d70d2057de24cda88c2b4dd

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -7,9 +7,9 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      uint32_t screen = inl(0x100);
-      info->width = screen >> 16;
-      info->height = screen & 0x0000ffff;
+      //uint32_t screen = inl(0x100);
+      info->width = 400;
+      info->height = 300;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }

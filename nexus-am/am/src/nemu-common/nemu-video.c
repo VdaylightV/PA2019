5ca20a4d55f3120abf4f5355053ac23661c84b64
tracer-ics2019 <tracer@njuics.org>
2019-11-18 09:13:18 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  09:13:18 up 4 days, 23:41, 14 users,  load average: 2.15, 1.80, 1.68 62c48db9d9687e874189e9e66ba16ed64a109346

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -29,7 +29,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
       int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
       int cp_bytes = sizeof(uint32_t) * min(w, 400 - x);
-      for (int j = 0; j < h && y + j < 300; j ++) {
+      for (int j = 0; j < h && y + j < 300; ++ j) {
         memcpy(&fb[(y + j) * 400 + x], pixels, cp_bytes);
         pixels += w;
       }

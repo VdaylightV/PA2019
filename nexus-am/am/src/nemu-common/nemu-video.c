8d4cd9b8eee1626c7200d8b9dd23d5d511a903f8
tracer-ics2019 <tracer@njuics.org>
2019-11-18 09:05:10 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  09:05:10 up 4 days, 23:33, 14 users,  load average: 1.69, 1.68, 1.59 8b0989e1e3952765b0e812a24469d8f1ba3936bb

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -7,7 +7,7 @@ static inline int min(int x, int y) {
   return (x < y) ? x : y;
 }
 
-static uint32_t* const fb __attribute__((used)) = (uint32_t *)0xa0000000;
+static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {

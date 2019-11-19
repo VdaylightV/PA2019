617d8753a383534c1bc817ff04f960c1b2c94fc8
tracer-ics2019 <tracer@njuics.org>
2019-11-19 22:56:40 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:56:40 up  6:35,  8 users,  load average: 0.87, 0.71, 0.54 120d8229fafbe75bebbad3089bf6f4f2b5cb46ec

--- nexus-am/tests/amtest/src/tests/video.c
+++ nexus-am/tests/amtest/src/tests/video.c
@@ -75,7 +75,6 @@ void video_test() {
 
   while (1) {
     unsigned long upt = uptime();
-    unsigned long a = 89123123;
     if (upt - last > 1000 / FPS) {
       update();
       redraw();
@@ -85,7 +84,6 @@ void video_test() {
     if (upt - fps_last > 1000) {
       // display fps every 1s
       printf("%d: FPS = %d\n", upt, fps);
-      printf("%d",a);
       fps_last = upt;
       fps = 0;
     }

6bc7fcc3ea1a39dd4313e2729801ed5ee51af3ac
tracer-ics2019 <tracer@njuics.org>
2019-11-20 08:52:47 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  08:52:47 up  7:05, 10 users,  load average: 0.96, 0.67, 0.58 1aa3aaec2f7c34c61e01e06629895f13a328bc8e

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -9,7 +9,7 @@ static inline int min(int x, int y) {
 }
 */
 
-//static uint32_t* const fb __attribute__((used)) = (uint32_t *)FB_ADDR;
+static uint32_t* const fb __attribute__((used)) = (uint32_t *)FB_ADDR;
 //static uint32_t fb[400 * 300] = {};
 
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
@@ -29,16 +29,16 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-/*      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
-      int cp_bytes = sizeof(uint32_t) * min(w, 400 - x);
+      int cp_bytes = sizeof(uint32_t) * (w < 400-w ? w : 400-w);
       for (int j = 0; j < h && y + j < 300; j ++) {
         memcpy(&fb[(y + j) * 400 + x], pixels, cp_bytes);
         pixels += w;
       }
-*/
+
     if (ctl->sync) {
-       outl(0x104, 0);
+//       outl(0x104, 0);
       }
       return size;
     }

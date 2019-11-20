d66628afeb0edcd59163c7fb332803e06d0fc22e
tracer-ics2019 <tracer@njuics.org>
2019-11-20 09:17:08 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  09:17:08 up  7:30,  9 users,  load average: 0.54, 0.51, 0.47 46c5d80672b810d63fe406c6fa49e69a4c5021a

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -16,9 +16,9 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      //uint32_t screen = inl(0x100);
-      info->width = 400;
-      info->height = 300;
+      uint32_t screen = inl(0x100);
+      info->width = screen >> 16;
+      info->height = screen << 16 >> 16;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -29,14 +29,20 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+ /*     int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
       int cp_bytes = sizeof(uint32_t) * (w < 400-w ? w : 400-w);
       for (int j = 0; j < h && y + j < 300; j ++) {
         memcpy(&fb[(y + j) * 400 + x], pixels, cp_bytes);
         pixels += w;
       }
+*/
 
+    int i;
+    for( i = 0; i < ctl->h; i ++) {
+
+      memcpy(fb+(ctl->y+i)*screen_width()+ctl->x, ctl->pixels+i*ctl->w,ctl->w*4);
+    }
     if (ctl->sync) {
 //       outl(0x104, 0);
       }

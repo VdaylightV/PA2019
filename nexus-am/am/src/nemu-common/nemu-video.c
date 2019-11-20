0f4ce1fca606116230e146d637ced63ab2485cba
tracer-ics2019 <tracer@njuics.org>
2019-11-20 18:52:09 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  18:52:09 up  8:48,  9 users,  load average: 1.17, 0.83, 0.75 7037a94208c65d08488df4942170fe5b0c02eb0

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -9,16 +9,16 @@ static inline int min(int x, int y) {
 }
 */
 
-static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
+//static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 //static uint32_t fb[400 * 300] = {};
 
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      uint32_t screen = inl(0x100);
-      info->width = screen >> 16;
-      info->height = screen << 16 >> 16;
+//      uint32_t screen = inl(0x100);
+      info->width = inw(SCREEN_ADDR+2);
+      info->height = inw(SCREEN_ADDR);
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -29,6 +29,18 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+    if (ctl->sync) {
+       outl(0x104, 0);
+      }
+    else {
+      int width = inw(SCREEN_ADDR + 2);
+      uint32_t *fb = (uint32_t*)(uintptr_t)FB_ADDR;
+      for(int i = 0; i <ctl->h; i ++) {
+        for(int j = 0; j < ctl->w; j ++) {
+          fb[(i + ctl->y)*width + j + ctl->x] = ctl->pixels[i*ctl->w + j];
+        }
+      }
+    }
  /*     int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
       int cp_bytes = sizeof(uint32_t) * (w < 400-w ? w : 400-w);
@@ -44,9 +56,6 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
       memcpy(fb+(ctl->y+i)*screen_width()+ctl->x, ctl->pixels+i*ctl->w,ctl->w*4);
     }
 */
-    if (ctl->sync) {
-       outl(0x104, 0);
-      }
       return size;
     }
   }

3ca7da173c4cc82f7666c888498bb3945be6e560
tracer-ics2019 <tracer@njuics.org>
2019-11-26 21:25:43 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:25:43 up 16:02,  8 users,  load average: 0.72, 0.67, 0.56 ba0a007e362558f5c0d94bacf196f79d2f28212e

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -13,7 +13,7 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
    s1 = vaddr_read(s0 + NO * 8, 4);
    s2 = vaddr_read(s0 + NO * 8 + 4, 4);
 
-   s0 = ((s2 & 0xffff0000) + (s1 & 0x0000ffff));
+   s0 = ((s2 & 0xffff0000) | (s1 & 0x0000ffff));
 
    rtl_push(&ret_addr);
    rtl_j(s0);

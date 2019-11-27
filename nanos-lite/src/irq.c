1fbc54e17ea6c74886cd61513f8c1c6cd2153b0d
tracer-ics2019 <tracer@njuics.org>
2019-11-27 16:52:35 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  16:52:35 up 18:40,  9 users,  load average: 0.71, 1.69, 1.91 61eb7e5d41ac51495972e3f6615a00331c5e354

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_YIELD: printf("F**k PA!!!!\n"); break;
-    default: panic("Unhandled event ID = %d", e.event);
+    default: panic("///////Unhandled event ID = %d", e.event);
   }
 
   return NULL;

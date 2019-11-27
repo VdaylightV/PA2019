84b0fea16cfd268fd26ed09fd7f423f4f93d9078
tracer-ics2019 <tracer@njuics.org>
2019-11-27 17:01:41 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  17:01:41 up 18:49,  9 users,  load average: 1.89, 2.25, 2.14 9f8ab6486739b22b28076d88c66291666655935

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_YIELD: printf("F**k PA!!!!\n"); break;
-    default: panic("Unhandled event ID = %d _EVENT_YIELD:%d", e.event, _EVENT_YIELD);
+    default: panic("Unhandled event ID = %d", e.event);
   }
 
   return NULL;
--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -26,7 +26,7 @@ _Context* __am_irq_handle(_Context *c) {
     _Event ev = {0};
     switch (c->irq) {
       case 5: {ev.event = _EVENT_YIELD; break;}
-      default:{ev.event = _EVENT_ERROR; break;}
+      default:{printf("############%d\n", c->irq); ev.event = _EVENT_ERROR; break;}
     }
 
     next = user_handler(ev, c);

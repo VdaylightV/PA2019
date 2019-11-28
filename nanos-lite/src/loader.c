4c7763162674feedaab62fefb624ecdcede30114
tracer-ics2019 <tracer@njuics.org>
2019-11-28 17:51:26 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  17:51:26 up 1 day,  2:08,  9 users,  load average: 0.51, 0.27, 0.22 4b0f57d0650213ad485051df2ac74562ba4f54a4

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -23,13 +23,11 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   for(uint16_t i = 0; i < elf_ehdr.e_phnum; i ++) {
       ramdisk_read(&elf_phdr, i * elf_ehdr.e_phentsize + sizeof(elf_ehdr), sizeof(elf_phdr));
 	  if(elf_phdr.p_type == PT_LOAD) {
-  printf("Ehdr Size:%d\n",sizeof(elf_ehdr));
-  printf("Phdr Entry Size:%d\n",elf_ehdr.e_phentsize);
           printf("Start of ELF_Header:0x%08x\n",&elf_ehdr);
           printf("Offset:0x%08x\n",elf_phdr.p_offset);
-		  printf("Start:0x%08x\n", &elf_ehdr + (elf_phdr.p_offset/52));
+		  printf("Start:0x%08x\n", &elf_ehdr + (elf_phdr.p_offset/sizeof(elf_ehdr)));
 		  void* ptr = (void*)elf_phdr.p_vaddr;
-	      memcpy(ptr, &elf_ehdr + (elf_phdr.p_offset / 52), elf_phdr.p_filesz);
+	      memcpy(ptr, &elf_ehdr + (elf_phdr.p_offset / sizeof(elf_ehdr)), elf_phdr.p_filesz);
 
 		  memset(&elf_ehdr + elf_phdr.p_offset + elf_phdr.p_filesz, '0', elf_phdr.p_memsz - elf_phdr.p_filesz + 1);
 	  }

f7322ec593eb318a377f511bca09e21947f83cf4
tracer-ics2019 <tracer@njuics.org>
2019-12-04 20:57:50 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:57:50 up 1 day, 21:33,  6 users,  load average: 0.91, 0.68, 0.46 4ee1e91cb98863e1e9ea36e23931443f6c55a552

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -22,7 +22,7 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
    offset2 = vaddr_read(cpu.idtr.base + NO * 8 + 4, 4);
    
    addr = ((offset2 & 0xffff0000) + (offset1 & 0x0000ffff));
-   assert(addr!=0);
+   //assert(addr!=0);
 
    printf("address head:0x%x\n", cpu.idtr.base);
    printf("NO:0x%x\n", NO);

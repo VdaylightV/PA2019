c8958e3e6102a1934ae90365b7051a3571cf6566
tracer-ics2019 <tracer@njuics.org>
2019-12-05 09:22:02 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  09:22:02 up 1 day, 23:23,  6 users,  load average: 0.63, 0.76, 0.84 387fab95a3d45cf2146ac59ad7ed35bc6018aa6a

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -430,7 +430,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 		   case 'x':
 				{ 
 				 int val = va_arg(ap, int);
-				 char temp[128] = "";
+				 char temp[65535] = "";
 				 char *head = temp;
 
 				 char *result = to_hex(val, head);

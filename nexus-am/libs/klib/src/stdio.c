135a81013760ec43f5b5a709c632bf7200e4c59d
tracer-ics2019 <tracer@njuics.org>
2019-12-05 10:49:43 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  10:49:43 up 2 days, 48 min,  6 users,  load average: 1.45, 0.87, 0.81 df1d778ac9a9d46fd2705a8d5a43c8e69438ac62

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -369,7 +369,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 //				 char* fill_width = "0";
 //				 size_t len_zero = strlen(fill_width);
-                 char val_str[56] = "";
+                 char val_str[64] = "";
 
 				 char* head_val_str = &val_str[0];
 

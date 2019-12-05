107b4aa63a36bc6f39560b0f99d44aa359a37991
tracer-ics2019 <tracer@njuics.org>
2019-12-05 12:38:33 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  12:38:33 up 2 days,  2:05,  6 users,  load average: 0.99, 0.55, 0.35 ba9b480e72f8f307fe20d51a2a7c257af08ca6bf

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,8 +2,8 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-    case _EVENT_YIELD: {printf("yield!!!!\n"); break;}
-    //case _EVENT_SYSCALL: {printf("syscall\n"); break;}
+    case _EVENT_YIELD: case _EVENT_SYSCALL: {printf("yield!!!!\n"); break;}
+//    case _EVENT_SYSCALL: {printf("syscall\n"); break;}
     default: panic("Unhandled event ID = %d", e.event);
   }
 
@@ -14,3 +14,4 @@ void init_irq(void) {
   Log("Initializing interrupt/exception handler...");
   _cte_init(do_event);
 }
+ 

d71fa8c1dce59d3e6b5503b1d81456c410b5f26c
tracer-ics2019 <tracer@njuics.org>
2019-12-12 15:03:25 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  15:03:25 up 14:52,  4 users,  load average: 1.87, 1.40, 1.52 c3ce99b132dd8d42721e1b986cb886f41aff91bb

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -19,17 +19,17 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-   int key = read_key();
-   if(key == _KEY_NONE) {
+   //int key = read_key();
+   //if(key == _KEY_NONE) {
        uint32_t time = uptime();
 	   int length = sprintf((char*)buf, "t %d\n", time);
 	   return length;
-   } 
+  // } 
 
-   else {
+   //else {
 	   
-	   char status = key & 0x8000 ? 'd': 'u';
-	   return sprintf((char*)buf, "k%c %s\n", status, keyname[key&0x7fff]);
+//	   char status = key & 0x8000 ? 'd': 'u';
+//	   return sprintf((char*)buf, "k%c %s\n", status, keyname[key&0x7fff]);
 	   
 	  /* 
        char flag;
@@ -44,7 +44,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 	   int length = sprintf((char*)buf, "k%c %s\n", flag, key_name);
        return length;
 	   */
-   }
+  // }
 
 
 }

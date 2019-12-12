cd63064a91a2f33d9cd54ca47a8b609bc94ae4e3
tracer-ics2019 <tracer@njuics.org>
2019-12-12 21:57:22 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:57:21 up 20:13,  7 users,  load average: 0.01, 0.04, 0.02 deb8ba4770c85031c215b916ed6d881dda48ef4a

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -4,7 +4,7 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 //static int i = 0;
-
+/*
 int fac(int base, int exp) {
     int result = 1;
 	for(int i = 0; i < exp; i ++) {
@@ -115,9 +115,9 @@ size_t str_to_int(char* str) {
 	}
 	return value;
 }
+*/
 
-
-
+/*
 int printf(const char *fmt, ...) {
 	//我的代码：
   //i ++;
@@ -379,6 +379,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   return 0;
 
 }
+*/
 
 /*
 void my_itoa(char *str, unsigned digit, int base) {
@@ -502,6 +503,143 @@ int sprintf(char *out, const char *fmt, ...) {
   return ret;
 }
 */
+
+int printf(const char *fmt, ...) {
+    va_list ap;
+	va_start(ap, fmt);
+#define MAX 200
+	char buf[200];
+	int i,n=vsnprintf(buf, -1, fmt, ap);
+	assert(n<MAX);
+#undef MAX
+	for(i = 0; i < n; ++ i) {
+	    _putc(buf[i]);
+	}
+	return i;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+    return vsnprintf(out, -1, fmt, ap);
+}
+
+int sprintf(char *out, const char * fmt, ...) {
+    va_list ap;
+	va_start(ap, fmt);
+	return vsnprintf(out, -1, fmt, ap);
+}
+
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+#define output(A) if(cnt<n-1){out[cnt++]=A;}else{va_end(ap);out[cnt]='\0';return cnt;}
+	size_t cnt = 0;
+	int i = 0;
+
+	const char *p, *sval;
+	char fill, num[10];
+	int ival,fill_width;
+
+	uintptr_t uval;
+	char cval;
+	for(p=fmt;*p!='\0';++p)
+	{
+		if(*p!='%') {
+			output(*p);
+			continue;
+		}
+		fill_width=0;
+		fill = ' ';
+		if(*p == '0') {
+		    fill = '0';
+			++p;
+		}
+        fill_width = 0;
+		++p;
+		while(*p >= '0' && *p <= '9') {
+		    fill_width *= 10;
+			fill_width += *p - '0';
+			++p;
+		}
+		switch(*p) {
+		    case 'c':
+				cval = va_arg(ap, int);
+				output(cval);
+				break;
+			case 'u':
+				uval = va_arg(ap, uint32_t);
+				i = 0;
+				while(uval > 0) {
+				    num[i++] = uval % 10 + '0';
+					uval /= 10;
+				}
+				while(fill_width > i) {
+				   num[i++] = fill;
+				}
+                if(i == 0) {
+				    output('0');
+				}
+				else {
+				   while(i > 0) {
+				      output(num[--i]);
+				   }
+				}
+				break;
+			case 'x':
+			case 'p':
+				uval = (uintptr_t) va_arg(ap, void*);
+				i = 8;
+				while(i>0) {
+				    output(
+							(uval >> ((sizeof(void*) << 3) -4)) < 10 ?
+							(uval >> ((sizeof(void*) << 3) -4)) + '0':
+							(uval >> ((sizeof(void*) << 3) -4)) - 10 + 'a');
+					uval <<= 4;
+					--i;
+
+				}
+				break;
+			case 'd':
+				ival = va_arg(ap, int);
+				if(ival < 0) {
+				   output('-');
+				   ival = -ival;
+				}
+				while(ival > 0) {
+				    num[i++] = ival % 10 + '0';
+					ival /= 10;
+				}
+				while (fill_width > i) {
+				    num[i++] = fill;
+				}
+				if(i == 0) {
+				    output('0');
+				} else {
+				    while(i>0) {
+					    output(num[--i]);
+					}
+				}
+				break;
+			case 's':
+				for(sval = va_arg(ap, char*); *sval != '\0'; ++sval) {
+				    output(*sval);
+				}
+				break;
+			default:
+				if(*p < '0' || *p > '9') {
+				    output(*p);
+					printf("%%%c not realized.\n");
+					assert(0);
+					cnt+=vsprintf(out+cnt, "not realized\n", ap);
+					break;
+				}
+
+		}
+	
+	}
+	va_end(ap);
+	output('\0');
+	return cnt-1;
+#undef output
+}
+
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   return 0;
 }

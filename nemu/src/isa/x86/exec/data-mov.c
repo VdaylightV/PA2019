94c7c1e2c5636a8431287b1f95229b84a733fae8
tracer-ics2019 <tracer@njuics.org>
2019-12-13 21:59:21 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:59:21 up 1 day,  6:39,  9 users,  load average: 1.10, 0.85, 0.97 dc12647eeafb38d7906bf52d4b649d63e8e1adbb

--- nemu/src/isa/x86/exec/data-mov.c
+++ nemu/src/isa/x86/exec/data-mov.c
@@ -124,19 +124,22 @@ make_EHelper(movs) {
   if(id_dest->width != 1) {
     id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
   }
- // s0 = cpu.esi;
+  s0 = cpu.esi;
   rtl_lm(&cpu.edi, &cpu.esi, id_dest->width);
-  //s0 = cpu.edi;
-  //rtl_lm(&s0, &s1, id_dest->width);
-  cpu.esi += id_dest->width;
-  cpu.edi += id_dest->width;
+  s0 = cpu.edi;
+  rtl_lm(&s0, &s1, id_dest->width);
+  //cpu.esi += id_dest->width;
+  rtl_lm(&cpu.edi, &cpu.esi, id_dest->width);
+  s0 = id_dest->width;
+  rtl_add(&cpu.edi, &cpu.edi, &s0);  
+  //cpu.edi += id_dest->width;
 
   /*
   id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
   rtl_lm(&cpu.edi, &cpu.esi, id_dest->width);
   s0 = id_dest->width;
-//  rtl_add(&cpu.edi, &cpu.edi, &s0);  
-//  rtl_add(&cpu.esi, &cpu.esi, &s0);  
+  rtl_add(&cpu.edi, &cpu.edi, &s0);  
+  rtl_add(&cpu.esi, &cpu.esi, &s0);  
   cpu.edi += s0;
   cpu.esi += s0;
   */

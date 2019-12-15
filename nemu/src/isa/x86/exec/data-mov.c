4b95526c6f557f75ab1ba82a7e232a0df5653316
tracer-ics2019 <tracer@njuics.org>
2019-12-15 18:26:03 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  18:26:03 up 1 day,  6:49,  9 users,  load average: 0.89, 1.21, 1.23 1faebc66166a9fde5016efb21dd51191bfa126a

--- nemu/src/isa/x86/exec/data-mov.c
+++ nemu/src/isa/x86/exec/data-mov.c
@@ -125,7 +125,8 @@ make_EHelper(movs) {
     id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
   }
   s0 = cpu.esi;
-  rtl_lm(&cpu.edi, &cpu.esi, id_dest->width);
+//  rtl_lm(&cpu.edi, &cpu.esi, id_dest->width);
+  rtl_lm(&s1, &s0, id_dest->width);
   s0 = cpu.edi;
   rtl_lm(&s0, &s1, id_dest->width);
 //  rtl_addi(&cpu.edi, &cpu.edi, s0);  

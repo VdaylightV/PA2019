b808fe91805b4b5bce282f618d72f504545449c4
tracer-ics2019 <tracer@njuics.org>
2019-12-16 20:52:56 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:52:56 up 1 day, 12:10,  6 users,  load average: 0.01, 0.18, 0.45 1bf49e2317e9d513254222858a234c3e5d8da61b

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -15,6 +15,15 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
+
+	  case CC_O: *dest = cpu.eflags.OF; break;
+	  case CC_B: *dest = cpu.eflags.CF; break;
+	  case CC_E: *dest = cpu.eflags.ZF; break;
+	  case CC_BE: *dest = ((cpu.eflags.CF) || (cpu.eflags.ZF)); break;
+	  case CC_S: *dest = cpu.eflags.SF; break;
+	  case CC_L: *dest = (cpu.eflags.SF != cpu.eflags.OF); break;
+	  case CC_LE: *dest = ((cpu.eflags.ZF) || (cpu.eflags.SF != cpu.eflags.OF)); break;
+
     /*
     case CC_O:  *dest = cpu.eflags.OF; break;
     case CC_NO: *dest = (cpu.eflags.OF == 0); break;
@@ -30,7 +39,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_LE:  *dest = ((cpu.eflags.SF ^ cpu.eflags.OF) | cpu.eflags.ZF); break;
     case CC_NLE:  *dest = ((cpu.eflags.SF == cpu.eflags.OF) && cpu.eflags.ZF == 0); break;
     */
-    
+   /* 
     case CC_O: {if (cpu.eflags.OF == 1) (*dest) = 1; else (*dest) = 0; break; }
     case CC_B: {if (cpu.eflags.CF == 1 && cpu.eflags.ZF == 0) (*dest) = 1; else (*dest) = 0; break; }
     case CC_E: {*dest = cpu.eflags.ZF; break; }
@@ -40,7 +49,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_LE: {if (cpu.eflags.ZF == 1 || cpu.eflags.SF != cpu.eflags.OF) (*dest) = 1; else (*dest) = 0; break; }
 	case CC_NE: {*dest = ~cpu.eflags.ZF; break; }
 	case CC_NL: {if (cpu.eflags.SF == cpu.eflags.OF || cpu.eflags.ZF == 1) (*dest) = 1; else (*dest) = 0; break; }
-
+     */
 //      TODO();
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");

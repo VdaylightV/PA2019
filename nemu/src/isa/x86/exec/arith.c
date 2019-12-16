64deb296c7489ff3438a00229fd555a3f9d07de0
tracer-ics2019 <tracer@njuics.org>
2019-12-16 22:27:03 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:27:03 up  1:03,  1 user,  load average: 0.54, 0.72, 0.71 9a22248dc48cab46107174269d63f9b944751e38

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -1,7 +1,6 @@
 #include "cpu/exec.h"
 
 make_EHelper(add) {
-  /*
     rtl_add(&s0, &id_dest->val, &id_src->val);
 	operand_write(id_dest, &s0);
 
@@ -16,9 +15,8 @@ make_EHelper(add) {
 
   rtl_is_add_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s1);
-*/
-
- // My code!!!
+/*
+ * My code!!!
   	rtl_add(&s0, &id_src->val, &id_dest->val);
   rtl_is_add_overflow(&s2, &s0, &id_src->val, &id_dest->val, id_src->width);
   rtl_set_OF(&s2);
@@ -26,14 +24,14 @@ make_EHelper(add) {
   rtl_set_CF(&s2);
   rtl_update_ZFSF(&s0, id_dest->width);
   operand_write(id_dest, &s0);
-
+*/
   //TODO();
 
   print_asm_template2(add);
 }
 
 make_EHelper(sub) {
-/*  
+  
   rtl_sub(&s0, &id_dest->val, &id_src->val);
   operand_write(id_dest, &s0);
 
@@ -48,10 +46,9 @@ make_EHelper(sub) {
 
   rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s1);
-*/
 
-  	
-  // My code!!!
+ /* 	
+  * My code!!!
   id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
   rtl_sext(&s1, &id_src->val, id_src->width);
    
@@ -62,7 +59,7 @@ make_EHelper(sub) {
   rtl_set_OF(&s2);
   rtl_update_ZFSF(&s0, id_dest->width);
   operand_write(id_dest, &s0);
-
+*/
 
 /*  printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@minused:%x\n", id_dest->val);
   printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@minus_before_extend:%x\n", id_src->val);
@@ -81,7 +78,7 @@ make_EHelper(sub) {
 }
 
 make_EHelper(cmp) {
-/*
+
   rtl_sub(&s0, &id_dest->val, &id_src->val);
 
   if(id_dest->width != 4) {
@@ -95,9 +92,8 @@ make_EHelper(cmp) {
 
   rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s1);
- */
-    
-   //My code !!!
+  /*  
+   *My code !!!
   id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;
   rtl_sext(&s1, &id_src->val, id_src->width);
   
@@ -108,7 +104,7 @@ make_EHelper(cmp) {
   rtl_is_sub_overflow(&s2, &s0, &id_dest->val, &id_src->val, id_src->width);
   rtl_set_OF(&s2);
   rtl_update_ZFSF(&s0, id_dest->width);
-  
+  */
 /*  printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@minused:%x\n", id_dest->val);
   printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@minus:%x\n", id_src->val);
   printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@result:%x\n", s0);
@@ -124,7 +120,7 @@ make_EHelper(cmp) {
 }
 
 make_EHelper(inc) {
-/*  rtl_li(&id_src->val, 1);
+  rtl_li(&id_src->val, 1);
   rtl_add(&s0, &id_dest->val, &id_src->val);
   if (id_dest->width != 4) {
     rtl_andi(&s0, &s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
@@ -135,9 +131,8 @@ make_EHelper(inc) {
   rtl_set_OF(&s1);
 
   operand_write(id_dest, &s0);
-*/
-  
-   //My code!!!
+  /*
+   *My code!!!
   s1 = 1; 
   rtl_add(&s0, &s1, &id_dest->val);
   rtl_is_add_overflow(&s2, &s0, &s1, &id_dest->val, id_dest->width);
@@ -146,14 +141,14 @@ make_EHelper(inc) {
   rtl_set_CF(&s2);
   rtl_update_ZFSF(&s0, id_dest->width);
   operand_write(id_dest, &s0);
-   
+   */
   //TODO();
 
   print_asm_template1(inc);
 }
 
 make_EHelper(dec) {
-/*
+
   rtl_li(&id_src->val, 1);
   rtl_sub(&s0, &id_dest->val, &id_src->val);
   operand_write(id_dest, &s0);
@@ -164,9 +159,8 @@ make_EHelper(dec) {
 
   rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
 rtl_set_OF(&s1);
-*/
-  
-   //My code
+  /*
+   *My code
   s1 = 1;
   rtl_sub(&s0, &id_dest->val, &s1);
   rtl_is_sub_overflow(&s2, &s0, &id_dest->val, &s1, id_dest->width);
@@ -176,12 +170,11 @@ rtl_set_OF(&s1);
   rtl_update_ZFSF(&s0, id_dest->width);
   operand_write(id_dest, &s0);
   //TODO();
-  
+  */
   print_asm_template1(dec);
 }
 
 make_EHelper(neg) {
-/*
   rtl_li(&id_src->val, 0);
   rtl_sub(&s0, &id_src->val, &id_dest->val);
   operand_write(id_dest, &s0);
@@ -196,9 +189,9 @@ make_EHelper(neg) {
   rtl_get_ZF(&s1);
   s1 = (s1 == 0); 
   rtl_set_CF(&s1);
-*/
-  
-   //My code!!!
+
+  /*
+   *My code!!!
 	//s0 = id_dest->val;
 	s1 = 1;
 	rtl_not(&s3, &id_dest->val);
@@ -215,7 +208,7 @@ make_EHelper(neg) {
 		rtl_set_CF(&s1);
 	}
     operand_write(id_dest, &s2);
-  
+  */
   //TODO();
 
   print_asm_template1(neg);

b04b9c0d7be967fbf31386be87dd328d4cf6f2fd
tracer-ics2019 <tracer@njuics.org>
2019-12-17 15:46:59 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  15:46:59 up  5:08,  1 user,  load average: 0.91, 0.65, 0.66 8a6649f4528b6cd9bab99d41a0b535bae7f44ef2

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -89,15 +89,17 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+
 //  int fd = fs_open("/dev/fb");
 //  fs_lseek(fd, offset, SEEK_SET);
 //  fs_write(fd, buf, len);
 //void draw_rect(uint32_t *pixels, int x, int y, int w, int h);
-    uint32_t *fb = (uint32_t*) buf;
+//My code!!!
+    //uint32_t *fb = (uint32_t*) buf;
     int x = (offset / 4) % screen_width();
 	int y = (offset / 4) / screen_width();
 	for(int i = 0; i < len / 4; i ++) {
-	   draw_rect(&fb[i], x, y, 1, 1);
+	   draw_rect(&((uint32_t*)buf)[i], x, y, 1, 1);
 	   if(x==screen_width()) {
 	       x = 0;
 		   y++;
@@ -106,15 +108,16 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 	       x++;
 	   }
 	  // x = ((x % screen_width())++);
-	  /*{
-	  int temp = x % screen_width();
-	  x = temp + 1;
-	   if(x == 0 && i > 0) {
-		   y ++;
-	   }
-	  }*/
+	  //{
+	  //int temp = x % screen_width();
+	  //x = temp + 1;
+	  // if(x == 0 && i > 0) {
+	  //	   y ++;
+	  // }
+	  //}
 	}
     return len;
+	
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {

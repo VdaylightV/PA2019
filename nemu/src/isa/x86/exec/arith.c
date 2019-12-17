7a783b35a7e041e0f11a2198e655ceb6e3fc1724
tracer-ics2019 <tracer@njuics.org>
2019-12-17 09:45:37 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  09:45:37 up  1:43,  1 user,  load average: 1.19, 0.97, 0.86 b33ff431d2e77112b2349e673c6b833faff26f28

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -33,7 +33,7 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  
+/*  
   rtl_sub(&s0, &id_dest->val, &id_src->val);
   operand_write(id_dest, &s0);
 
@@ -48,7 +48,7 @@ make_EHelper(sub) {
 
   rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s1);
-
+*/
   	
   //* My code!!!
 //  id_dest->width = decinfo.isa.is_operand_size_16 ? 2 : 4;

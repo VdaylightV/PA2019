430c923aaea943d1f79ac5538b208b214a87e4fb
tracer-ics2019 <tracer@njuics.org>
2019-12-18 21:09:51 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  21:09:51 up 10:25,  3 users,  load average: 0.81, 0.46, 0.34 4644a4a57a3fe063ef1f353d8085acfb5414d300

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -74,23 +74,27 @@ int _write(int fd, void *buf, size_t count) {
   //在return0时会输出很多“HHHHHH”
 }
 
-/*My code!!!
+//*My code!!!
 extern char _end;
 char* end = &_end;
-*/
+
+/*
 extern char _end;
 intptr_t p_break = (intptr_t)&_end;
+*/
 //放到_sbrk里会导致end时局部变量，就无法修改program_break的值
 void *_sbrk(intptr_t increment) {
-  //char *old_break = p_break;
-  intptr_t old_break = p_break;
-  //p_break = old_break + increment;
-  if (_syscall_(SYS_brk, p_break + increment, 0, 0) == 0)
+	/*
+  char *old_break = end;
+  //intptr_t old_break = p_break;
+  end = old_break + increment;
+  if (_syscall_(SYS_brk, end , 0, 0) == 0)
   {
     return (void *)old_break;
   }
   return (void *)-1;
-	/*
+  */
+	
   intptr_t program_break = end + increment;
      
   int ret = _syscall_(SYS_brk, program_break, 0, 0);
@@ -102,7 +106,7 @@ void *_sbrk(intptr_t increment) {
   else{
     return (void *)-1;
   }
-  */
+  
  /*My code!!!
   char *old_break = end;
   end = old_break + increment;

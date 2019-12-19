3c60425a796f6d8b5a29cbba6270d4b1ac779053
tracer-ics2019 <tracer@njuics.org>
2019-12-19 20:17:48 +0800

    >  run 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:17:48 up 16:18,  8 users,  load average: 0.31, 0.33, 0.40 28bfef4f5a459a2f927634b5568e4ded1739df09

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,18 +3,15 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-	int i = 0;
-	while(s[i] != '\0') ++i;
-	return i;
-/******GC***
+//******GC***
   size_t count = 0;
   while(s[count] != '\0') {
 	  count ++;
   }
 //  printf("Length:%d\n",count);
   return count;
-************
-*/
+//************
+
 /*
     size_t size = 0;
 	while(*(s++)) {
@@ -25,10 +22,6 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char* dst,const char* src) {
-	char* iter = dst;
-	while((*iter++ = *src++)) {
-	}
-	return dst;
 /*//#####CO###
   size_t i = 0;
   while(*(src+i) != '\0') {
@@ -39,7 +32,7 @@ char *strcpy(char* dst,const char* src) {
   return dst;
 *///##########
 
-/******GC***
+//******GC***
 	if (src == NULL) {
 		assert(0);
 	    return NULL;
@@ -60,17 +53,19 @@ char *strcpy(char* dst,const char* src) {
 	dst[length] = '\0';
 
 	return dst;
-************/
+//************/
 //	return strncpy(dst, src, strlen(src));
 
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
+	/*
 	size_t dest_len = strlen(dst);
 	size_t i = 0;
 	for(i = 0; i < n && src[i]!='\0'; ++i) dst[dest_len+i] = src[i];
 	dst[dest_len+i] = '\0';
 	return dst;
+    */
 
 /*/#####CO###
   size_t i = 0;
@@ -78,7 +73,7 @@ char* strncpy(char* dst, const char* src, size_t n) {
   for(; i < n; ++i) dst[i] = '\0';
   return dst;
 *///##########
-/******GC***
+//******GC***
 	if (src == NULL) {
 		assert(0);
 	    return NULL;
@@ -140,7 +135,7 @@ char* strncpy(char* dst, const char* src, size_t n) {
 
 		return dst;
 	}
-***********/
+//***********/
 
 /*
 
@@ -160,11 +155,13 @@ char* strncpy(char* dst, const char* src, size_t n) {
 }
 
 char* strcat(char* dst, const char* src) {
+/*
 	char* iter = dst;
 	while((*iter)) ++iter;
 	while((*iter++ = *src++)) {
 	}
 	return dst;
+*/
 /*//#####CO###
   size_t dst_len = strlen(dst);
   size_t i = 0;
@@ -175,7 +172,7 @@ char* strcat(char* dst, const char* src) {
   dst[dst_len + i] = '\0';
   return dst;
 *///##########
-/******GC***	
+//******GC***	
 	char* temp_dst = dst;
 	while(*temp_dst != '\0') {
 	    temp_dst ++;
@@ -196,7 +193,7 @@ char* strcat(char* dst, const char* src) {
 	temp_dst[length] = '\0';
 
     return dst;
-*************/
+//*************/
 
 /*/#####CO###The first version of correct 
 	char* result = dst;
@@ -218,7 +215,9 @@ char* strcat(char* dst, const char* src) {
 }
 
 int strcmp(const char* s1, const char* s2) {
+	/*
 	return strncmp(s1, s2, -1);
+	*/
 /*//#####CO###
   int l1 = strlen(s1);
   int l2 = strlen(s2);
@@ -232,7 +231,7 @@ int strcmp(const char* s1, const char* s2) {
   }
   return 0;
 *///##########
-/*****GC***
+//*****GC***
 	int len_s1 = strlen(s1);
 	int len_s2 = strlen(s2);
 
@@ -298,7 +297,7 @@ int strcmp(const char* s1, const char* s2) {
 	    return flag;
 	    
 	}
-*********/
+//*********/
 
 /*
 	int flag = 0;
@@ -312,6 +311,7 @@ int strcmp(const char* s1, const char* s2) {
 */
 }
 int strncmp(const char* s1, const char* s2, size_t n) {
+	/*
 	for(int i = 0; i < n; i ++) {
 	    if(s1[i]==s2[i]&&s1[i]) {
 		    continue;
@@ -321,6 +321,7 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 		}
 	}
 	return 0;
+	*/
 /*//#####GC##
   for(size_t i = 0; i < n; ++i) {
       if(s1[i] <s2[i]) return -1;
@@ -328,7 +329,7 @@ int strncmp(const char* s1, const char* s2, size_t n) {
   }
   return 0;
 *///#########
-/*****GC***
+//*****GC***
 	size_t len_s1 = strlen(s1);
 	size_t len_s2 = strlen(s2);
 
@@ -423,7 +424,7 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 		
 		}
 	}
-************/
+//************/
 
 }
 

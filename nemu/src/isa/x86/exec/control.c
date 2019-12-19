86a43074cb4e324f61176c7ebcfff049294c75fc
tracer-ics2019 <tracer@njuics.org>
2019-12-19 20:01:23 +0800

    >  compile 181240014 nector Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:01:23 up 16:02,  8 users,  load average: 0.36, 0.45, 0.47 a23373f2cd8b427ea2d4a0ae043a9c5704e312a

--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -37,14 +37,10 @@ make_EHelper(call) {
 
 make_EHelper(ret) {
   
-  rtl_pop(&s0);
-  rtl_j(s0); 
   
-  
-  /*My code !!! 
   rtl_pop(&decinfo.jmp_pc);
   rtl_j(decinfo.jmp_pc);
-  */
+
   //rtl_mv(&cpu.esp, &cpu.ebp);
   /* 
   rtl_lm(&s0, &cpu.esp, 4);
@@ -67,15 +63,10 @@ make_EHelper(ret_imm) {
 
 make_EHelper(call_rm) {
   
-  s0 = decinfo.seq_pc;
-  rtl_push(&s0);
-  rtl_j(id_dest->val);
-  
   //TODO();
   
-  /*My code!!!
   rtl_push(&decinfo.seq_pc);
   rtl_jr(&id_dest->val);
-  */
+
   print_asm("call *%s", id_dest->str);
 }
